6e9af9c3a170f5c44fd0cdddd8aa3d60f54c4a03
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-02-17 21:15:24 +0500

    Comments introduced in the code

--- StudentProfileManagementSystem/StudentProfileManagementSystem.cpp
+++ StudentProfileManagementSystem/StudentProfileManagementSystem.cpp
@@ -1,50 +1,50 @@
 #include "stdafx.h"
-#include<iostream>
-#include<conio.h>
-#include<iomanip>
-#include<stdio.h>
-#include<fstream>
-#include<string>
+#include<iostream>	// for input and output
+#include<conio.h>	//for getting character
+#include<iomanip>	//for setting width
+#include<stdio.h>	// for seperate build-in functions
+#include<fstream>	//for creating files for students
+#include<string>	// for string input
 using namespace std;
 
 class Student
 {
 private:
-	string fname;
-	string mname;
-	string lname, address;
-	string filename;
-	char Dept[10];
-	int Enroll;
-	int stdId;
-	short age, day, year, month;
+	string fname;			//First name
+	string mname;			//Middle name
+	string lname, address;	//Last name and address
+	string filename;		//Student file name
+	char Dept[10];			//Student's department
+	int Enroll;				//student's Enrollment Year
+	int stdId;				//student's ID
+	short age, day, year, month;	//student's DOB
 
 public:
-	Student();
-	~Student();
-	void Input();
-	void Search();
-	void Edit();
-	void DisplayAll();
-	void Delete();
+	Student();		//constructor
+	~Student();		//destructor
+	void Input();	//For registration 
+	void Search();	//for searching student's information
+	void Edit();	//for editing a current data
+	void DisplayAll(); //Displays all  student data
+	void Delete();	//delete current student data
 };
 
 int main()
 {
-	system("cls");
+	system("cls");	//clears console screen
 	system("Title Student Profile Management System");
-	system("color 03");
-	cout << "\t\t********************************************\n";
-	cout << "\t\t*       WELLCOME TO MEGA SOLUTION          *\n";
-	cout << "\t\t********************************************\n";
+	system("color 03");	//set color of text in console
+	cout << "\t\t*********************************************\n";
+	cout << "\t\t*       WELLCOME TO MEGA SOLUTIO8N          *\n";
+	cout << "\t\t*********************************************\n";
 	cout << "\t\t=============================================\n";
 	cout << "\t\t     Student Profile Management System\n";
 	cout << "\n\t\t\tEnter choices from below: " << endl;
 	cout << "1.Input new Student " << endl;
 	cout << "2.View an Student Data by ID number " << endl;
-	cout << "3.Edit an existing Student Data " << endl;
+	cout << "3.Edit an existing Student's Data " << endl;
 	cout << "4.Display all Student." << endl;
-	cout << "5.Delete an existing Student Data. " << endl;
+	cout << "5.Delete an existing Student's Data. " << endl;
 	cout << "6.Exit \n" << endl;
 	Student S;
 	int choice;
@@ -88,9 +88,9 @@ Student::~Student()
 }
 void Student::Input()
 {
-	system("cls");
-	system("color 08");
-	cout << "Enter Student ID: ";
+	system("cls");	//clears console screen
+	system("color 08");	//changes text color
+	cout << "Enter Student ID: ";		
 	cin >> stdId;
 	cout << "Enter First Name: ";
 	cin >> fname;
@@ -111,40 +111,40 @@ void Student::Input()
 	cin >> Enroll;
 	cout << "Enter Department: ";
 	cin >> Dept;
-	string stdId2 = to_string(stdId);
-	stdId2.append(".txt");
+	string stdId2 = to_string(stdId);	//changes stdID from int to string
+	stdId2.append(".txt");				//adds ".txt" to end ofstring
 
-	ofstream student(stdId2);
+	ofstream student(stdId2);			//creates student data to text file
 	student << fname << " " << mname << " " << lname << " " << stdId << " " << Enroll << " " << age << " " << day << setw(2) << month
-		<< " " << year << " " << Dept << endl;
-	student.close();
-	ofstream dir("Directory.txt", ios::app);
+		<< " " << year << " " << Dept << endl;	//inputs data to file
+	student.close();							//close the created file
+	ofstream dir("Directory.txt", ios::app);	//create backup for student's data
 	dir << fname << " " << mname << " " << lname << "   " << stdId << "   " << Enroll << " " << age << " " << day << " " << month
-		<< " " << year << "  " << Dept << endl;
-	dir.close();
+		<< " " << year << "  " << Dept << endl;	//inputs data to file
+	dir.close();	//closes backup file
 }
 void Student::Search()
 {
 	system("cls");
 	system("color 06");
-	cout << "Enter Student ID number: ";
+	cout << "Enter Student ID number: ";	//needs ID for searching
 	cin >> stdId;
 	string stdId2 = to_string(stdId);
 	stdId2.append(".txt");
 	ifstream student(stdId2);
-	while (student >> fname >> mname >> lname >> stdId >> Enroll >> age >> day >> month >> year >> Dept)
+	while (student >> fname >> mname >> lname >> stdId >> Enroll >> age >> day >> month >> year >> Dept)	//Takes the entire data in the file
 	{
-		int mnameLength = mname.size();
-		int lnameLength = lname.size();
-		int fnameLength = fname.size();
-		int TotalLength = fnameLength + mnameLength + lnameLength;
+		int mnameLength = mname.size();	//size of middle name 
+		int lnameLength = lname.size(); //size of last name
+		int fnameLength = fname.size(); //size of first name
+		int TotalLength = fnameLength + mnameLength + lnameLength; // adds the size of name
 
 		string answer;
-		cout << "Is this correct Student[y/n] " << endl;
+		cout << "Is this correct Student[y/n] " << endl; // if correct data shown?
 		cout << fname << " " << mname << " " << lname << " " << stdId << " " << age << " " << Dept << endl;
 		cin >> answer;
 
-		if (answer == "y")
+		if (answer == "y")	//if yes then perform following
 		{
 			system("cls");
 			cout << "Name";
@@ -238,7 +238,7 @@ void Student::DisplayAll()
 	system("color 03");
 	cout << "Entire Student Database " << endl;
 	cout << "------------------------ " << endl;
-	ifstream dir("Directory.txt");
+	ifstream dir("Directory.txt"); //checks backup file and prints all the data available in it.
 	system("cls");
 	cout << "Name";
 	for (int y = 1; y < 18; y++)
@@ -270,18 +270,20 @@ void Student::Delete()
 	system("cls");
 	system("color B");
 	int File;
-	cout << "Enter Student ID to delete the Data: ";
+	cout << "Enter Student ID to delete the Data: ";	//File of student to be deleted
 	cin >> File;
 
 	string empId = to_string(File);
 	empId.append(".txt");
-	const char *mycharp = empId.c_str();
+	const char *mycharp = empId.c_str();	//converst string to constant *char
 
-	if (remove(mycharp) != 0)
+	if (remove(mycharp) != 0)	//if file not available 
 		cout << "Remove operation failed" << endl;
-	else
+	else						//if file available
 	{
-		cout << File << " has been removed." << endl;
-		_sleep(5000);
+		cout << File << " is being remove." << endl;
+		_sleep(5000);	// time delay for pc
 	}
+	cout << File << " is Removed\n";
+	getch();
 }
